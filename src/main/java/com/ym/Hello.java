1782bd36d62cf0a55e33265d02e599acbfdad2d7
YanM <yanm_eng@foxmail.com>
2020-05-27 12:28:13 +0800

    第一次提交

---
+++ src/main/java/com/ym/Hello.java
@@ -0,0 +1,14 @@
+package com.ym;
+
+/**
+ * @ClassName Hello
+ * @Description: TODO
+ * @Author YanM
+ * @Date 2020/5/27
+ * @Version V1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
